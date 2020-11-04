298ca9ceb331854ae4dd43c444cd7126370906d0
Taha Ben Masaud <tbm206@gmail.com>
2020-11-04 20:52:59 +0000

    P1.0

--- src/VirtualDOM/DOM.purs
+++ src/VirtualDOM/DOM.purs
@@ -7,11 +7,14 @@ import Prelude
 import Effect (Effect)
 import Web.DOM.Document as Doc
 import Web.DOM.Node as Node
+import Web.DOM.NodeList as NodeList
+import Web.DOM.Text as Text
 import Web.DOM.Internal.Types (Node)
 import Web.DOM.Element (toNode)
 import Web.HTML as HTML
 import Web.HTML.HTMLDocument (toDocument)
 import Web.HTML.Window as Window
+import Data.Maybe (Maybe)
 
 document :: Effect Doc.Document 
 document = HTML.window >>= Window.document >>= toDocument >>> pure
@@ -19,5 +22,14 @@ document = HTML.window >>= Window.document >>= toDocument >>> pure
 createElement :: String -> Effect Node
 createElement name = document >>= Doc.createElement name >>= toNode >>> pure
 
+createTextNode :: String -> Effect Node
+createTextNode text = document >>= Doc.createTextNode text >>= Text.toNode >>> pure
+
 replaceChild :: Node -> Node -> Node -> Effect Unit
 replaceChild new old parent = void $ Node.replaceChild new old parent
+
+removeChild :: Node -> Node -> Effect Unit
+removeChild child parent = void $ Node.removeChild child parent
+
+childAt :: Int -> Node -> Effect (Maybe Node)
+childAt index node = Node.childNodes node >>= NodeList.item index
