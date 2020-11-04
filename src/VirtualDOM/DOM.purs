51842eda37d409204dfe87b66d2a0f15bb4ac1a2
Taha Ben Masaud <tbm206@gmail.com>
2020-11-04 19:40:20 +0000

    build ok

--- src/VirtualDOM/DOM.purs
+++ src/VirtualDOM/DOM.purs
@@ -4,6 +4,20 @@ module VirtualDOM.DOM
   ) where
 
 import Prelude
+import Effect (Effect)
 import Web.DOM.Document as Doc
+import Web.DOM.Node as Node
+import Web.DOM.Internal.Types (Node)
+import Web.DOM.Element (toNode)
+import Web.HTML as HTML
+import Web.HTML.HTMLDocument (toDocument)
+import Web.HTML.Window as Window
 
-document :: 
+document :: Effect Doc.Document 
+document = HTML.window >>= Window.document >>= toDocument >>> pure
+
+createElement :: String -> Effect Node
+createElement name = document >>= Doc.createElement name >>= toNode >>> pure
+
+replaceChild :: Node -> Node -> Node -> Effect Unit
+replaceChild new old parent = void $ Node.replaceChild new old parent
